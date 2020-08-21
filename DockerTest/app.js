4645fa84c99353e6a16e205a7900ea0d5f2a5030
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-08-21 09:31:11 +0700

    added docker tutorials

---
+++ DockerTest/app.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello Nicha'))
+app.listen(3000, () => console.log('My REST API running on port 3000!'))
